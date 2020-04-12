dfc7d3e715ac1eff29d2aa5ca9f1ea6487fe6315
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-04-12 12:40:15 -0400

    set up switch/route on app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import {Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
+       <Navbar/>
+       <Switch>
+          <Route exact path="/" render={props => <Home {...props} />}/> {/* says if url is homepage (/) then just show the home */}
+
+      </Switch>
+    
     </div>
   );
 }
