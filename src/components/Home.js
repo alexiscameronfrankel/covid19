da72fc1c2b0d2708be5c0b055ca0101ed77aca34
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-05-10 23:54:32 -0400

    working on mobile responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,13 +16,13 @@ class Home extends Component {
               </section>
               <section id="about_home"> 
                 <h1>About the Social Support Platform</h1>
-                <article>
+                <div>
                     There are thousands of resources created to address COVID-19 all over the world. We hope not to contribute to ever growing list of tools, trackers, and never ending feeds. Instead, we want to help you navigate all of those resources and help you connect with people along the way. 
                     <br/><br/>
                     We believe that together we can survive the pandemic.
                     <br/><br/>
                     <b>Covid-19, Simplified</b> was created by a group of public health professionals with expertise in health behaviors, peer and social support, and community development. The contents of this resource are grounded in research and best practices.
-                </article>
+                </div>
                 <button id="about_home_btn" type="button">Read More</button>
               </section>
             </>
