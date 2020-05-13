c25ac0c539a45f9d71d1281c00820e35522eeb21
alexiscameronfrankel <alexiscameronfrankel@gmail.com>
2020-05-12 23:57:57 -0400

    working on second section

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,14 +16,14 @@ class Home extends Component {
               </section>
               <section id="about_home"> 
                 <h1>About the Social Support Platform</h1>
-                <div>
+                <article>
                     There are thousands of resources created to address COVID-19 all over the world. We hope not to contribute to ever growing list of tools, trackers, and never ending feeds. Instead, we want to help you navigate all of those resources and help you connect with people along the way. 
                     <br/><br/>
                     We believe that together we can survive the pandemic.
                     <br/><br/>
                     <b>Covid-19, Simplified</b> was created by a group of public health professionals with expertise in health behaviors, peer and social support, and community development. The contents of this resource are grounded in research and best practices.
-                </div>
-                <button id="about_home_btn" type="button">Read More</button>
+                </article>
+                <button id="about_home_btn" type="button">Read more about the evidence</button>
               </section>
             </>
         );
